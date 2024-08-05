30d47f3c7c9e35618fbc872d788851ea6699a323
Ashish singh <ashishsingh1331@gmail.com>
2024-08-05 15:25:16 +0530

    Pages directory routing

---
+++ src/pages/folder/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function FolderIndex() {
+  return (
+    <div>
+      FolderIndex
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
