ccdfd9c026b646dabaa9622701064408aed05c48
Ashish singh <ashishsingh1331@gmail.com>
2024-08-05 15:32:26 +0530

    Pages directory metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>This is home page title</title>
+      </Head>
       <h1>Home</h1>
+      <NestedComponent />
+    </>
+  );
+}
+
+function NestedComponent() {
+  return (
+    <>
+      <Head>
+        <title>This is new title</title>
+      </Head>
+      <h2>Nested</h2>
     </>
   );
 }
